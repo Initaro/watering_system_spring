cf765b3434a80b8cf2fb56fb32df38a62d935370
Initaro <72886971+Initaro@users.noreply.github.com>
2022-03-02 13:07:46 +0200

    Added test controller

--- src/main/java/com/example/watering_system/data/repository/UserRepository.java
+++ src/main/java/com/example/watering_system/data/repository/UserRepository.java
@@ -3,5 +3,4 @@ package com.example.watering_system.data.repository;
 import com.example.watering_system.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
+public interface UserRepository extends JpaRepository<User, Integer> {}
