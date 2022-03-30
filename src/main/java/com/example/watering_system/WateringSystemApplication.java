da3469dbd7c153876b41fcc79f6119e8aed42133
Initaro <72886971+Initaro@users.noreply.github.com>
2022-03-30 18:07:41 +0300

    First schedule time add

--- src/main/java/com/example/watering_system/WateringSystemApplication.java
+++ src/main/java/com/example/watering_system/WateringSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.watering_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WateringSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WateringSystemApplication.class, args);
