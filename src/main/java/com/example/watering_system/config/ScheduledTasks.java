bbe8e133569928b97f48f4c935a70e5c248ce4b9
Initaro <72886971+Initaro@users.noreply.github.com>
2022-04-21 12:16:18 +0300

    Added scheduler for getting sensor data

--- src/main/java/com/example/watering_system/config/ScheduledTasks.java
+++ src/main/java/com/example/watering_system/config/ScheduledTasks.java
@@ -3,6 +3,7 @@ package com.example.watering_system.config;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.example.watering_system.SpringHttpClient.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,10 +13,17 @@ import org.springframework.stereotype.Component;
 public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    //private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 1000)
-    public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+    public void restClient() {
+        System.out.println("Works!!!");
+        RestClient restClient = new RestClient();
+
+        restClient.get();
     }
+
+    /*public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }*/
 }
